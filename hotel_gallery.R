dd2a11362ebf20fa8611f099011b96f518d55605
KamilKandzia <36645098+KamilKandzia@users.noreply.github.com>
2020-05-18 20:22:59 +0200

    Modified version of the gallery
    Background transparent + auto-size of images

--- hotel_gallery.R
+++ hotel_gallery.R
@@ -5,9 +5,7 @@ hotel_gallery <- function(id, label = "hotel_gallery1") {
   mainPanel(width=12,
             fixedRow(
               h1("Hotel gallery!"),
-              p("After several hours searching for a solution related to the white frame at the pictures, I capitulated. Today's sprint (as they say in an Agile) was not successful, and I was defeated by CSS, which despite modifications still doesn't look right. Maybe it's time to accept the default background as white..."),
-              tags$a(href="https://stackoverflow.com/questions/17290701/maintain-image-aspect-ratio-in-carousel", "Stack"),
-              tags$a(href="https://stackoverflow.com/questions/58359294/make-bootstrap-carousel-background-transparent", "Another Stack"),
+              p("slickR package has been modified to makes background transparent and keeps aspect ratio"),
               slickROutput(ns("slickr")))
             )
 
